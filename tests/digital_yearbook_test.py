e066821a01aa0dda6af59cf9fa756291143fbf5f
Canyon Smith <canyonfsmith@gmail.com>
2023-10-16 14:53:21 -0600

    Replaced Selenium with Playwright for browser automation

--- tests/digital_yearbook_test.py
+++ tests/digital_yearbook_test.py
@@ -1,27 +1,27 @@
 import pytest
 from dotenv import load_dotenv
 from os import environ as env
-from selenium.webdriver import Chrome
+from playwright.sync_api import sync_playwright
 
 load_dotenv()
 
 @pytest.fixture(scope="session")
 def browser():
-    """Setup and teardown for Chrome browser."""
-    driver = Chrome()
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
+    """Setup and teardown for browser."""
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        yield page
+        browser.close()
 
-def login_to_culture_cloud(browser, url, username, password):
+def login_to_culture_cloud(page, url, username, password):
     """Login to Culture Cloud with given credentials."""
-    browser.get(url)
-    browser.find_element('id', 'usernameField').send_keys(username)
-    browser.find_element('id', "nextButton").click()
-    browser.find_element('id', "passwordField").send_keys(password)
-    browser.find_element('id', "signInButton").click()
+    page.goto(url)
+    page.fill('#usernameField', username)
+    page.click('#nextButton')
+    page.fill('#passwordField', password)
+    page.click('#signInButton')
 
 def test_login_to_culture_cloud(browser):
     """Test the login functionality of Culture Cloud."""
     login_to_culture_cloud(browser, env['CULTURE_CLOUD_URL'], env["AUTOMATION_USERNAME"], env['AUTOMATION_USER_PASSWORD'])
-#END
