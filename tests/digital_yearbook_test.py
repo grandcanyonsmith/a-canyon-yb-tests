43d3b437dcaa4403b462cac56b4f0731f9ee2c1a
Canyon Smith <canyonfsmith@gmail.com>
2023-10-16 14:25:12 -0600

    updated tests/digital_yearbook_test.py

--- tests/digital_yearbook_test.py
+++ tests/digital_yearbook_test.py
@@ -7,12 +7,14 @@ load_dotenv()
 
 @pytest.fixture(scope="session")
 def browser():
+    """Setup and teardown for Chrome browser."""
     driver = Chrome()
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
 
 def login_to_culture_cloud(browser, url, username, password):
+    """Login to Culture Cloud with given credentials."""
     browser.get(url)
     browser.find_element('id', 'usernameField').send_keys(username)
     browser.find_element('id', "nextButton").click()
@@ -20,4 +22,6 @@ def login_to_culture_cloud(browser, url, username, password):
     browser.find_element('id', "signInButton").click()
 
 def test_login_to_culture_cloud(browser):
+    """Test the login functionality of Culture Cloud."""
     login_to_culture_cloud(browser, env['CULTURE_CLOUD_URL'], env["AUTOMATION_USERNAME"], env['AUTOMATION_USER_PASSWORD'])
+#END
