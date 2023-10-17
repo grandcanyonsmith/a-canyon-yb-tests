050c1997a9097362e65ce45c5527bd55d1d995a1
Canyon Smith <canyonfsmith@gmail.com>
2023-10-17 02:29:10 -0600

    Added docstrings to Config class and its methods

--- tests/legacy/ui_tests/config.py
+++ tests/legacy/ui_tests/config.py
@@ -2,14 +2,17 @@ import configparser
 
 
 class Config:
+    """Class to handle configuration files."""
     _config = configparser.ConfigParser()
 
     @classmethod
     def load(cls, filename="config.ini"):
+        """Load the configuration file."""
         cls._config.read(filename)
 
     @classmethod
     def get_section(cls, section):
+        """Get a specific section from the configuration file."""
         try:
             return dict(cls._config.items(section))
         except configparser.NoSectionError:
