574f71b92b650c17d5b59b6e9ecebdc053736aa2
Canyon Smith <canyonfsmith@gmail.com>
2023-10-22 10:27:08 -0600

    updated tests/celebrations/ui_tests/tests/conftest.py

--- tests/celebrations/ui_tests/tests/conftest.py
+++ tests/celebrations/ui_tests/tests/conftest.py
@@ -1,30 +1,41 @@
 import os
-
-from dotenv import *
+from dotenv import load_dotenv
 import pytest
-
 from testrail import testrail_helper as tr
 from playwright.sync_api import sync_playwright
 
+# Constants
+BASE_URL = 'https://octanner.testrail.io'
+USERNAME = os.getenv('TESTRAIL_USERNAME')
+PASSWORD = os.getenv('TESTRAIL_PASSWORD')
+PROJECT_ID = 33
+SUITE_ID = 2775
+RUN_NAME = 'Automated Celebrations Health Check'
+
 load_dotenv()
 
-base_url = 'https://octanner.testrail.io'
-username = os.getenv('TESTRAIL_USERNAME')
-password = os.getenv('TESTRAIL_PASSWORD')
-project_id = 33
-suite_id = 2775
-run_name = 'Automated Celebrations Health Check'
-
-def setup_before_tests():
-    test_run_id = tr.create_test_run(base_url, username, password, project_id, suite_id, run_name)
+def create_test_run():
+    """
+    Create a test run and set the id in the environment variables
+    """
+    test_run_id = tr.create_test_run(BASE_URL, USERNAME, PASSWORD, PROJECT_ID, SUITE_ID, RUN_NAME)
     os.environ["test_run_id"] = f"{test_run_id}"
-    pass
+    print("Test run created with id: ", test_run_id)
+
+def close_test_run():
+    """
+    Close the test run
+    """
+    tr.close_test_run(os.getenv("test_run_id"), BASE_URL, USERNAME, PASSWORD)
+    print("Test run closed")
 
 @pytest.fixture(scope='session', autouse=True)
-def after_all_tests():
-    setup_before_tests()
+def run_tests():
+    """
+    Setup and teardown for tests
+    """
+    print("Setting up tests")
+    create_test_run()
     yield
-
-    tr.close_test_run(os.getenv("test_run_id"), base_url, username, password)
-
-    print("Finished running after all tests")
+    print("Tearing down tests")
+    close_test_run()
